80ea0a78e9939745c2049ecdba309313e39ce3e5
Levi J. Hall <home-made@live.com>
2023-07-18 23:43:26 -0400

    Genius Lyrics

--- spotify_app/lyrics.py
+++ spotify_app/lyrics.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import requests
 import logging
+import datetime
+import string
 
 from bs4 import BeautifulSoup
 from urllib.parse import quote_plus
 from difflib import SequenceMatcher
+from lyricsgenius import Genius
 
 from langdetect import detect
 from langdetect import DetectorFactory
@@ -54,12 +57,14 @@ def string_likeness(main: str, second: str) -> float:
         return (1. * match.size) / max_length
 
 
-def find_lyrics(band: str, song: str, min_similarity=0.5) -> str:
+def freak_lyrics(session: requests.Session,
+                 band: str, song: str, min_similarity=0.5) -> tuple:
+
     root_url = "https://www.lyricsfreak.com"
     search_stem = "/search.php?q="
 
     url = root_url + search_stem + quote_plus(song)
-    response = requests.get(url)
+    response = session.get(url)
     if response.status_code != 200:
         print(response.status_code, url)
         return [], -200, url
@@ -111,10 +116,71 @@ def find_lyrics(band: str, song: str, min_similarity=0.5) -> str:
         if line:
             lyrics.append(line)
 
-    return lyrics, max_score, url
+    return (lyrics, max_score, url)
 
 
-def search(search_terms: pd.DataFrame, max_searches=10_000, verbose=False):
+def genius_lyrics(genius: Genius, artist: list,
+                  band: str, song: str, min_similarity=0.5) -> tuple:
+
+    def clean_genuis_lyrics(lyrics: str) -> str:
+        lyrics_start = lyrics.find('Lyrics')
+        newline_start = lyrics.find('\n')
+        if lyrics_start == -1 or lyrics_start > newline_start:
+            lyrics_start = 0
+        else:
+            lyrics_start = lyrics_start + len('Lyrics')
+
+        lyrics_end = lyrics.rfind('You might also like')
+        newline_end = lyrics.rfind('\n')
+        if lyrics_end == -1 or lyrics_end < newline_end:
+            lyrics_end = len(lyrics)
+
+        cleaned = lyrics[lyrics_start:lyrics_end]
+        cleaned = cleaned.removeprefix('\n')
+        cleaned = cleaned.removesuffix('Embed')
+        cleaned = cleaned.rstrip(string.digits)
+
+        return cleaned
+
+    if not artist:
+        artist_search = genius.search_artist(band, include_features=True)
+        if artist_search is not None:
+            artist.append(artist_search)
+    elif band != artist[0].name:
+        artist.clear()
+        artist_search = genius.search_artist(band, include_features=True)
+        if artist_search is not None:
+            artist.append(artist_search)
+
+    if not artist:
+        return ('', -4, '')
+
+    closest_song = None
+    max_score = 0
+
+    for track in artist[0].songs:
+        score = string_likeness(song, track.title)
+        if score < min_similarity:
+            continue
+        if score > max_score:
+            max_score = score
+            closest_song = track
+        if score == 1:
+            break
+
+    if max_score >= min_similarity:
+        lyrics = clean_genuis_lyrics(closest_song.lyrics)
+        url = closest_song.url
+    else:
+        return ('', -4, '')
+
+    return (lyrics, max_score, url)
+
+
+def search_gen(search_terms: pd.DataFrame,
+               max_searches=10_000, verbose=False):
+
+    session = requests.Session()
     searches = 0
 
     try:
@@ -133,16 +199,18 @@ def search(search_terms: pd.DataFrame, max_searches=10_000, verbose=False):
                 continue
 
             if searches >= max_searches:
-                break
+                yield searches
+                searches = 0
             searches += 1
 
-            lyrics, status, url = find_lyrics(band, song)
+            lyrics, status, url = freak_lyrics(session, band, song)
 
             if status <= -200:
                 logging.critical("No longer returning 200\n" /
                                  f"{band}, {song}, {status}, {url}")
                 print("No longer returning 200")
-                return searches
+                yield searches
+                return
 
             # print(status, url)
 
@@ -159,6 +227,22 @@ def search(search_terms: pd.DataFrame, max_searches=10_000, verbose=False):
     except Exception as e:
         logging.critical(f"{str(e)}\n")
         print(str(e))
-        return searches
+        yield searches
+        return
 
-    return searches
+    yield searches
+    return
+
+
+if __name__ == '__main__':
+    start_time = datetime.datetime.now()
+    search_terms = pd.read_pickle('data/final/lyrics.pkl')
+    total_searches = 0
+    for searches in search_gen(search_terms, 10, verbose=True):
+        total_searches = total_searches + searches
+        if total_searches > 30:
+            break
+    search_terms.to_pickle('data/final/lyrics.pkl')
+    end_time = datetime.datetime.now()
+
+    print(end_time - start_time)
